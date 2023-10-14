0e2422171bfc8db108fa50980f7b50aa798066cc
nonaaaaaaaaa <nehalserag44@gmail.com>
2023-10-14 08:29:42 -0700

    need editting

--- main.h
+++ main.h
@@ -1,11 +1,21 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _PRINTF_H
+#define _PRINTF_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <limits.h>
 #include <unistd.h>
+
+#define OUTPUT_BUF_SIZE 1024
+#define BUF_FLUSH -1
+
+#define NULL_STRING "(null)"
+
+#define PARAMS_INIT {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+
+#define CONVERT_LOWERCASE 1
+#define CONVERT_UNSIGNED  2
 
 /**
  * struct format - struct format
